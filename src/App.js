b0a8bd03918a123ce0130e0d4af87c3ca6008279
Dirk Hornung <dirkhornung91@gmail.com>
2018-10-11 18:05:40 +0200

    added authenticator

--- src/App.js
+++ src/App.js
@@ -1,59 +1,19 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
-import { DatePicker } from 'antd'
-import MessengerOptIn from './components/messengerOptIn'
-import MessengerSDK from './components/messengerSDK.js'
+import authenticator from './services/authenticator'
 import './App.css'
-
-/* (function(d, s, id){
- *   var js, fjs = d.getElementsByTagName(s)[0];
- *   if (d.getElementById(id)) {return;}
- *   js = d.createElement(s); js.id = id;
- *   js.src = "//connect.facebook.net/en_US/messenger.Extensions.js";
- *   fjs.parentNode.insertBefore(js, fjs);
- * }(document, 'script', 'Messenger'));
- * 
- * window.extAsyncInit = function() {
- *   // the Messenger Extensions JS SDK is done loading
- *   console.log('window', window)
- *   window.MessengerExtensions.getContext('598231856876775',
- *     function success(thread_context){
- *       console.log(thread_context)
- *     },
- *     function error(err){
- *       console.error(err)
- *     }
- *   );
- * }; */
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            {this.props.psid}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <DatePicker />
-          <MessengerOptIn
-            pageId='515235208535564'
-            appId='598231856876775'
-            dataRef="test"
-          />
-          <MessengerSDK />
         </header>
       </div>
     );
   }
 }
 
-export default App;
+export default authenticator(App);
---
+++ src/services/authenticator.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+
+const authenticator = (WrappedComponent) => {
+  return class MessengerOptIn extends Component {
+    state = {
+      loading: true,
+      psid: null
+    }
+
+    componentDidMount() {
+      /* if (document.getElementById('facebook-jssdk')) {
+       *   return;
+       * } */
+      this.setFbAsyncInit();
+      this.loadSdkAsynchronously();
+    }
+
+    setFbAsyncInit = () => {
+      const self = this
+      window.extAsyncInit = function() {
+        window.MessengerExtensions.getContext(
+          '598231856876775',
+          function success(ctx) {
+            self.setState({ loading: false, psid: ctx.psid })
+          },
+          function error(err){
+            console.error(err)
+          }
+        );
+      };
+    }
+
+    loadSdkAsynchronously() {
+      /* eslint-disable */
+      (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/messenger.Extensions.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'Messenger'))
+      /* eslint-enable */
+    }
+
+    render() {
+      if(this.state.loading)
+        return (<div>Loading...</div>)
+
+      return (
+        <WrappedComponent psid={this.state.psid} />
+      )
+    }
+  }
+}
+
+export default authenticator
